22bfe46944ad7b68ee20d599e77e9d206e10d82c
gilleygoat <rob.n.gilley@gmail.com>
2021-07-11 19:20:05 -0700

    Fuck Yeah # 2

--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return JsonResponse(requests.get('https://professional.app/wp-json/wp/v2/entity?slug=goat&_fields=id').content.decode("ascii"), safe=False)
