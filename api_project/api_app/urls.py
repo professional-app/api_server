83e7859a3fb93b39164315e6190fa514ba949747
gilleygoat <rob.n.gilley@gmail.com>
2021-08-12 16:40:26 -0700

    first git new branch

--- api_project/api_app/urls.py
+++ api_project/api_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('wolf', views.wolf, name='wolf'),
+    path('pages', views.pages, name='pages'),
+    path('entity', views.entity, name='entity'),
+    #path('list_relationships', views.list_relationships, name='list_relationships'),
 ]
--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -5,11 +5,15 @@ import requests
 
 from django.http import HttpResponse, JsonResponse
 
-def anything(something):
-    return  JsonResponse(requests.get(f'https://professional.app/wp-json/wp/v2/{something}').content.decode("ascii"), safe=False)
 
-def index(request):
-    return anything('pages')
+def make_api_call(path):
+    return  JsonResponse(requests.get(f'https://professional.app/wp-json/wp/v2/{path}').content.decode("ascii"), safe=False)
 
-def wolf(request):
-    return anything('entity')
+#def list_relationships(request):
+#    return make_api_call(f'entity?include={entity_id}&_fields=relationships')
+
+def pages(request):
+    return make_api_call('pages')
+
+def entity(request):
+    return make_api_call('entity')
