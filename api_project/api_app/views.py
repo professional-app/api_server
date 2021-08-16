bcde94874ff3e88b6df3443c976ae01bc60a1d81
eons <eons44@users.noreply.github.com>
2021-08-15 20:35:39 -0700

    Adds draft of entity query

--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -3,13 +3,55 @@ from django.shortcuts import render
 # Create your views here.
 import requests
 
-from django.http import HttpResponse, JsonResponse
+import eons, esam
+import json
 
-def anything(something):
-    return  JsonResponse(requests.get(f'https://professional.app/wp-json/wp/v2/{something}').content.decode("ascii"), safe=False)
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+
+def get_remote_api(query):
+    return requests.get(f'https://professional.app/wp-json/wp/v2/{query}').content.decode("ascii")
+
+class Entity(esam.Datum):
+    def __init__(self, id=0, name=eons.INVALID_NAME()):
+        super().__init__(name)
+        self.uniqueId = id
+        self.image_url = ""
+        self.contact_url = ""
+        self.description = ""
+        self.activities = []
+        self.interests = []
+        self.solicitations = []
+        self.relationships = {}
+
+def get_remote_entity(entity_id=0):
+    entity = Entity()
+    if not entity_id:
+        entity.Invalidate()
+        return entity
+
+    try:
+        entity_json = json.loads(get_remote_api(f"entity?include={entity_id}"))[0]
+
+        entity.uniqueId = entity_json["id"]
+        entity.name = entity_json["title"]["rendered"]
+        for i in entity_json["interests"]:
+            entity.interests.append(i["post_title"])
+        for a in entity_json["activities"]:
+            entity.activities.append(a["post_title"])
+
+        entity.MakeValid()
+        return entity
+
+    except Exception as e:
+        entity.Invalidate()
+        return entity
 
 def index(request):
-    return anything('pages')
+    entity = get_remote_entity(request.GET["entity"])
+    if entity.IsValid():
+        return JsonResponse(entity.ToJSON(), safe=False)
+    else:
+        return HttpResponseBadRequest("Invalid entity request.")
 
 def wolf(request):
     return anything('entity')
