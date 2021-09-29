555088d37d3bb3e548b4000fbb2e140d949ef693
eons <eons@eons.dev>
2021-09-28 17:31:57 -0700

    Debugging docker errors: removed exec statement

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,5 +9,4 @@ cd $API_PATH
 #Do this every time, even though its wasteful on startup.
 python manage.py makemigrations
 python manage.py migrate --noinput
-
-exec "$(which python) ${API_PATH}/manage.py $@"
+python manage.py $@
